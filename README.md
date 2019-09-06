fa92a0d987df4825108764bc914c8f2b0d56e5e8
tdevm <cr14b17@gmail.com>
2019-09-07 02:00:16 +0530

    bootstrapped

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-A React Boilerplate 
-
-React App written and build from scratch using Webpack, Babel.
-
-When you need granular control of your configuration and project structure.
+Onbo Web
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import FormContainer from "./components/container/FormContainer.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const element = () => {
+    return <h1>Scratch</h1>
+};
+
+ReactDOM.render(element(), document.getElementById("react-root"));
